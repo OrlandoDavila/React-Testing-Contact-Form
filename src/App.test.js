8ae4a32b1dbae975d0bd1e1b6a71adf9d6859b37
Orlando Davila <odavila0308@gmail.com>
2020-08-07 10:09:04 -0400

    mvp complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("Adds information to list when form is submitted", async() => {
+  
+  render(<App />);
+  
+  // const h1 = screen.getByText(/A Form to Beat All Others!/i);
+  const firstNameInput = screen.getByLabelText(/first Name/i);
+  const lastNameInput = screen.getByLabelText(/last Name/i);
+  const emailInput = screen.getByLabelText(/email/i);
+
+
+  fireEvent.change(firstNameInput, { target: { value: 'Orlando' } })
+  fireEvent.change(lastNameInput, { target: { value: 'Davila' } })
+  fireEvent.change(emailInput, { target: { value: 'oly@gmail.com' } })
+
+  const submitBtn = screen.getByRole('button', { name: /submit/i});
+  
+  fireEvent.click(submitBtn);
+
+  expect(await screen.findByText(/Orlando/i)).toBeInTheDocument();
+})
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,13 +12,15 @@ const ContactForm = () => {
 
   return (
     <div className="App">
+      <h1 className="formTitle">A Form to Beat All Others!</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id="firstName"
             name="firstName"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, maxLength: 8 })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -28,6 +30,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -41,7 +44,7 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id="email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
